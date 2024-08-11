b83ff9e504ac3a530256843aa2dfc11814332222
sungup <wsup97@gmail.com>
2024-08-11 19:02:54 +0900

    ✨ feat[#4]: 가장 최근에 멘토 등록한 3명 조회하는 메서드 제작

--- src/main/java/com/haribo/business/profile/domain/repository/MentoRepository.java
+++ src/main/java/com/haribo/business/profile/domain/repository/MentoRepository.java
@@ -2,7 +2,10 @@ package com.haribo.business.profile.domain.repository;
 
 import com.haribo.business.profile.application.dto.MentoRDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
@@ -10,4 +13,7 @@ public interface MentoRepository extends JpaRepository<MentoRDto, String> {
 
     boolean existsByProfileId(String profileId);
     MentoRDto findByProfileId(String profileId);
+
+    @Query(value = "SELECT * FROM mentomember ORDER BY regist_date DESC LIMIT 3", nativeQuery = true)
+    List<MentoRDto> findTop3ByOrderByregistDatetDesc();
 }
